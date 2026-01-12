d5c3f2f0143cc55310c3f955a88fc805a57e5092
Mayur303 <mgccomputer2311@gmail.com>
2026-01-12 16:02:25 +0530

    Setup of React frontend and implemented user login today

---
+++ client/src/pages/Login.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { loginUser } from "../services/api";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await loginUser({ email, password });
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("role", res.data.role);
+      alert("Login Successful");
+    } catch (err) {
+      alert("Login Failed");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
